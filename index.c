e8493ba1523f56f886ad9e33990a4737154416c1
kaemy <130376746+mariadesouzaw1@users.noreply.github.com>
2023-06-07 12:51:07 -0300

    save

--- index.c
+++ index.c
@@ -1,6 +1,23 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+    int tutorial(){
+        
+    }
+    int escrever_noticia(){
+        printf("Categorias:\n1 - Esportes\n2 - Saude\n3 - Educacao\n4 - sla");
+    }
+int main() {
+    int escolha1_2;
+    printf("****NOTICIÁRIO****\n");
+    printf("Digite 1 para escrever uma nova notícia ou 2 para observar as notícias ja publicadas:\n---> ");
+    scanf("%d",&escolha1_2);
+    if (escolha1_2==1){
+        
 
-printf("oi");
-
+    }
+    else if(escolha1_2==2){
+        
+    }else{
+        printf("resposta invalida");
+    }
+    
 }
